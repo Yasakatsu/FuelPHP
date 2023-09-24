48bceddfe31361c5534509bcc5766bfa6dae518e
Yasakatsu <chappieoct4@gmail.com>
2023-09-24 15:27:43 +0900

    fix db.php

--- fuel/app/config/db.php
+++ fuel/app/config/db.php
@@ -1,52 +1,31 @@
 <?php
 
-/**
- * Fuel is a fast, lightweight, community driven PHP 5.4+ framework.
- *
- * @package    Fuel
- * @version    1.9-dev
- * @author     Fuel Development Team
- * @license    MIT License
- * @copyright  2010 - 2019 Fuel Development Team
- * @link       https://fuelphp.com
- */
-
-/**
- * -----------------------------------------------------------------------------
- *  Global database settings
- * -----------------------------------------------------------------------------
- *
- *  Set database configurations here to override environment specific
- *  configurations
- *
- */
-
 return array(
-  'active' => 'testdb',
-  /**
-   * Base config, just need to set the DSN, username and password in env. config.
-   */
-  'default' => array(
-    'type'        => 'pdo',
-    'connection'  => array(
-      'persistent' => false,
+  'active' => 'mysqli',
+
+  'pdo' => array(
+    'type'           => 'pdo',
+    'connection'     => array(
+      'dsn'        => 'mysql:host=localhost;dbname=framework',
+      'username'       => 'root',
+      'password'       => 'root',
+      'persistent'     => false,
+      'compress'       => false,
     ),
-    'identifier'   => '`',
     'table_prefix' => '',
     'charset'      => 'utf8',
-    'enable_cache' => true,
-    'profiling'    => false,
+    'caching'      => false,
+    'profiling'    => true,
   ),
-
-  'testdb' => array(
-    'type'   => 'mysqli',
-    'connection' =>
-    array(
-      'hostname'   => 'localhost',
-      'database'   => 'データベース名',
-      'username'   => 'ユーザ名',
-      'password'   => 'パスワード',
-      'persistent' => FALSE,
+  'mysqli' => array(
+    'type'           => 'mysqli',
+    'connection'     => array(
+      'hostname' => 'localhost',
+      'database' => 'framework',
+      'username'       => 'root',
+      'password'       => 'root',
+      'persistent'     => false,
+      'compress'       => false,
     ),
     'table_prefix' => '',
     'charset'      => 'utf8',
