f323773efe256e16f8e8b59c81c4e1f30a7d914e
Yasakatsu <chappieoct4@gmail.com>
2023-09-29 08:19:06 +0900

    fix footer.php

--- fuel/app/views/template/footer.php
+++ fuel/app/views/template/footer.php
@@ -1,3 +1,17 @@
 <div class="footer">
-  &copy; Copyright <?php echo date('Y'); ?>
+    &copy; Copyright Tsukasa Osanai <?php echo date('Y'); ?>, ALL Right Reserved.
 </div>
+<script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
+<?= Asset::js('fixfooter.js') ?>
+
+<script>
+    $(function() {
+        let $toggleMsg = $('.js-toggle-msg');
+        if ($toggleMsg.length) {
+            $toggleMsg.slideDown();
+            setTimeout(function() {
+                $toggleMsg.slideUp();
+            }, 3000);
+        }
+    });
+</script>
