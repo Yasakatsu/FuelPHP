b3d48794bd9f9b927f7f831391a839183d12700c
Yasakatsu <chappieoct4@gmail.com>
2023-09-30 08:19:51 +0900

    fix head.php

--- fuel/app/views/template/head.php
+++ fuel/app/views/template/head.php
@@ -1,7 +1,3 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- cssの読み込みはheadタグ内で行う -->
-<?php echo Asset::css('bootstrap.css'); ?>
-<link rel="icon" href="<?php echo Asset::get_file('favicon.ico', 'img', 'icons/'); ?>" type="image/png">
-
-<title>ページタイトルです。</title>
+<meta charset="utf-8">
+<title>ホームページのタイトルが入ります。</title>
+<?php echo Asset::css('style.css') ?>
