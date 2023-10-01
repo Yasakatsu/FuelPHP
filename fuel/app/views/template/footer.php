902b3006bc9ab67498c19a1fe5fe92c0c81bbfd2
Yasakatsu <chappieoct4@gmail.com>
2023-10-01 18:08:21 +0900

    add signup.php

--- fuel/app/views/template/footer.php
+++ fuel/app/views/template/footer.php
@@ -1,4 +1,4 @@
-<div class="footer">
+<div id="footer">
     &copy; Copyright Tsukasa Osanai <?php echo date('Y'); ?>, ALL Right Reserved.
 </div>
 <script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
